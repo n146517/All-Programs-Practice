ffeedf3555f7360159510e7b6afc57a6856c3cae
Nishant Katiyar <nishantkatiyar5@gmail.com>
2021-12-26 14:59:46 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace abc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            try
+            {
+                String name = null;
+                Console.WriteLine(name.Length);
+            }
+            catch (NullReferenceException ex)
+            {
+                Console.WriteLine("String is null");
+                Console.WriteLine(ex.Message);
+            }
+            Console.ReadLine();
+        }
+    }
+}
