ce8813ff434f1154ce213732191e9c15f4cae8c2
Nishant Katiyar <nishantkatiyar5@gmail.com>
2021-12-26 12:06:50 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace factorial_number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int i, fact = 1, N;
+
+            Console.Write("Enter any Number: ");
+            N = int.Parse(Console.ReadLine());
+            for (i = 1; i <= N; i++)
+            {
+                fact = fact * i;
+            }
+            Console.Write("Factorial of " + N + " is: " + fact);
+
+
+        }
+
+
+    }
+}
