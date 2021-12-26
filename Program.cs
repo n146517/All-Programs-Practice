7007e50d66b39ba722be7675612e95a3499c3b6d
Nishant Katiyar <nishantkatiyar5@gmail.com>
2021-12-26 12:10:27 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace prime_number
+
+{
+    class Program
+    {
+        public static void Main(String[] args)
+        {
+            int N, i;
+
+            Console.WriteLine("Enter the Number");
+            N = int.Parse(Console.ReadLine());
+
+            for (i = 2; i <= N - 1; i++)
+
+                if (N % i == 0)
+                    break;
+
+            if(i==N)
+            {
+                Console.WriteLine("it is a prime number");
+            }
+            else
+            {
+                Console.WriteLine("it is not prime number");
+            }
+
+        }
+    }
+}
+
