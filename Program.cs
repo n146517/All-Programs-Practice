e5c14d33b84c6789f8aa0dbb1ee96fa0f542f2f4
Nishant Katiyar <nishantkatiyar5@gmail.com>
2021-12-26 15:06:04 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace overflow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+
+            Console.WriteLine("Enter the Number");
+
+            try
+            {
+                n = int.Parse(Console.ReadLine());
+                Console.WriteLine(n);
+            }
+            catch (Exception e)
+            {
+
+                Console.WriteLine(e);
+            }
+        }
+    }
+}
