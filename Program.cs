4dae577fb1bd307c81dfb340b95a2fb6742c1e68
Nishant Katiyar <nishantkatiyar5@gmail.com>
2021-12-26 15:10:13 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+
+namespace fileHandling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //if file is already existing in folder and i want to create new file with same name the get the exception
+            string path = @"F:\abc.txt";
+
+            try
+            {
+                FileStream fs = new FileStream(path, FileMode.CreateNew);
+                Console.WriteLine("File Created");
+                fs.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
