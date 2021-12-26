882bc10ba767d4c3e38e5b6ff75f67d485e6642e
Nishant Katiyar <nishantkatiyar5@gmail.com>
2021-12-26 15:01:02 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace abc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1, n2, divide;
+
+            try
+            {
+                Console.WriteLine("Enter the first number");
+                n1 = int.Parse(Console.ReadLine());
+                Console.WriteLine("Enter the second number");
+                n2 = int.Parse(Console.ReadLine());
+
+                divide = n1 / n2;
+                Console.WriteLine(divide);
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine("get the error divide by zero");
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
