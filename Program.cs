8236979a883ba5d40e248144b28b1e1cb317a3bd
Nishant Katiyar <nishantkatiyar5@gmail.com>
2021-12-26 14:56:12 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace indexoutofrange
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[5];
+            try
+            {
+                arr[0] = 5;
+                arr[1] = 4;
+                arr[2] = 3;
+                arr[3] = 6;
+                arr[4] = 8;
+                arr[5] = 7;
+
+                foreach (int i in arr)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            catch (IndexOutOfRangeException ex)
+            {
+                Console.WriteLine("Array is out of range");
+                Console.WriteLine(ex.Message);
+            }
+            Console.ReadLine();
+        }
+    }
+}
