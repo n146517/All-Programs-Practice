0af3c8c353bfa3d140ab30afcd89634a2c8550cf
Nishant Katiyar <nishantkatiyar5@gmail.com>
2021-12-26 15:04:07 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+
+namespace abc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string text = File.ReadAllText(@"F:\abcd1.txt");
+                Console.WriteLine(text);
+            }
+            catch (FileNotFoundException ex)
+            {
+                Console.WriteLine("File is not there");
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
