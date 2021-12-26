dcfeb7a4006a1f7845bb4f24cbc013e9ac62489c
Nishant Katiyar <nishantkatiyar5@gmail.com>
2021-12-26 12:14:51 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace sum_of_digit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+           int N, sum = 0, r;
+           Console.Write("Enter a number: ");      
+           N= int.Parse(Console.ReadLine());   
+            
+           while(N>0)      
+           {      
+              r = N%10;      
+              sum = sum+r;      
+              N = N/10;      
+            }
+           Console.Write("Sum is= "+sum);       
+       }  
+    }
+}
