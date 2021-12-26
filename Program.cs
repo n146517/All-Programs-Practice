f7b89c7a2ae7b30657276f17f2e75a2f611ad6c4
Nishant Katiyar <nishantkatiyar5@gmail.com>
2021-12-26 12:12:35 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace reverse_number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N, sum = 0, r;
+            Console.Write("Enter a number: ");
+            N = int.Parse(Console.ReadLine());
+            while (N != 0)
+            {
+                r = N % 10;
+                sum = sum * 10 + r;
+                N = N / 10;
+            }
+            Console.Write("Reversed Number: " + sum);
+        }
+    }
+}
