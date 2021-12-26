845794dda94f7ae65d7b2747d6f6436331d8530b
Nishant Katiyar <nishantkatiyar5@gmail.com>
2021-12-26 22:15:46 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+namespace GeeksForGeeks
+{
+
+	class abc
+	{
+
+		public delegate void add(int a, int b);
+		public delegate void sub(int a, int b);
+
+		public void addition(int a, int b)
+		{
+			Console.WriteLine(a + b);
+		}
+
+		public void subtract(int a, int b)
+		{
+			Console.WriteLine(a - b);
+		}
+
+		public static void Main(String[] args)
+		{
+
+			abc obj = new abc();
+
+			add obj1 = new add(obj.addition);
+			sub obj2 = new sub(obj.subtract);
+
+			obj1(100, 40);
+			obj2(100, 60);
+
+		}
+	}
+}
