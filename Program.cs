f9dbfa5b533332e5da37e69cb501e85103e15d81
Nishant Katiyar <nishantkatiyar5@gmail.com>
2021-12-26 13:10:33 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace DefaultConstractor
+{
+    class addition
+    {
+        int a, b,c,z;
+        public addition()   //default contructor
+        {
+            a = 100;
+            b = 175;
+            c = a + b;
+            Console.WriteLine("Default Constructor is:" +c);
+        }
+        public addition(int x, int y)  // Paremetrized Constructor
+        {
+            z = x + y;
+            Console.WriteLine("Paremetrized Constructor:" + z);
+        }
+
+        public static void Main()
+        {
+            addition obj = new addition();
+            addition addition = new addition(3, 5);
+            Console.ReadLine();
+        }
+    }
+}
