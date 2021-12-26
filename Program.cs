a7bef0e0c20d9ea008935e009579eb17a193ad9f
Nishant Katiyar <nishantkatiyar5@gmail.com>
2021-12-26 12:13:49 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Sort_an_array_in_descending_order
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                int[] arr = new int[] { 1, 2, 3, 4, 5 };
+                int length = arr.Length - 1;
+                string strReverse = null;
+                while (length >= 0)
+                {
+                    strReverse = strReverse + arr[length];
+                    length--;
+                }
+                Console.WriteLine();
+                Console.WriteLine("Reverse Array is " + " " + strReverse);
+                Console.ReadLine();
+            }
+        }
+    }
+}
