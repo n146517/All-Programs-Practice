2fae1314d1b1153c98807353630392282f781eef
Nishant Katiyar <nishantkatiyar5@gmail.com>
2021-12-26 22:57:05 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+
+namespace GeeksForGeeks
+{
+
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			BitArray b1 = new BitArray(8);
+			byte[] a = { 60 };
+			b1 = new BitArray(a);
+			Console.WriteLine(“Output of b1 60"); 
+		foreach (bool c in b1)
+			{
+				Console.Write(c);
+			}
+		}
+	}
+
+}
