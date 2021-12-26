5c5770013838e34b579aabd8a96ed555c9064940
Nishant Katiyar <nishantkatiyar5@gmail.com>
2021-12-26 14:58:22 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace abc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number");
+
+            try
+            {
+                int n = int.Parse(Console.ReadLine());
+                Console.WriteLine(n);
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine("String format is invalid");
+                Console.WriteLine(ex.Message);
+            }
+
+        }
+    }
+}
