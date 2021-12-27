ac7760771fb87caf4bbe75e51b91d6d8d19b06e3
Nishant Katiyar <nishantkatiyar5@gmail.com>
2021-12-27 13:07:13 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+public class FibonacciExample
+{
+    public static void Main(string[] args)
+    {
+        int a=-1, b=1,c, i, n;
+        Console.Write("Enter the number of elements: ");
+        n = int.Parse(Console.ReadLine());
+       
+        
+        for (i = 2; i < n; ++i) 
+        {
+            c = a + b;
+            Console.Write(c);
+            a = b;
+            b = c;
+        }
+    }
+}
